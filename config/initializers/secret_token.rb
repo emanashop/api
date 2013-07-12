3303454883883bc18cf35510aed517d36d0a45d2
vagrant <vagrant@lucid64.hsd1.ca.comcast.net>
2013-07-12 19:41:31 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emanashop::Application.config.secret_token = 'd9b71375e2dc8401456bc527dcf1d73ad47a5cd34da101c9ae02aa0dc58c9ac556d7dd0a3f602c41dfa495ede7ffbf9b61be9abd277af88d1d1c610e93a18025'
